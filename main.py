8b3b38f9cf5f1cebc950a1d8f4f82642ec3a9167
Ancelotow <ancelotow@gmail.com>
2023-10-12 15:03:12 +0200

    Linear regression ✨

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
+import numpy as np
 from sklearn import linear_model
 
-DAYS = [365, 1, 0]
+DAYS = [365, 14, 7, 3, 2, 1, 0]
 
-def createDataset():
+
+def create_dataset():
     data = []
     with open("temperature.txt") as f:
         for line in f.readlines():
@@ -19,7 +20,33 @@ def createDataset():
     return dataset
 
 
+def forcasting(dataset):
+    inputs = []
+    outputs = []
+    for data in dataset:
+        line_input = []
+        for i in range(len(data) - 1):
+            line_input.append(data[i])
+        inputs.append(line_input)
+        outputs.append(data[len(data) - 1])
+
+    reg = linear_model.LinearRegression()
+    reg.fit(inputs, outputs)
+
+    error = 0.0
+    for i in range(len(inputs)):
+        prediction = reg.predict([inputs[i]])
+        error += abs(prediction - outputs[i])
+    error = error / len(inputs)
+
+    return reg, error
+
+
+
 if __name__ == '__main__':
-    print(createDataset())
+    dataset = create_dataset()
+    reg, error = forcasting(dataset)
+    print(f'a={reg.coef_} b=†{reg.intercept_}')
+    print(f'error={error}')
 
 
